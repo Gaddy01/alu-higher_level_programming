c10a20a77b81f345a0663bf481d5bbdcb3c7dbc5
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-14 15:16:28 -0700

    task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 #str = str[45:72]+" "+str[88:92]+" "+str[0:6]
-print(str[45:71] + " " + str[88:92] + " " + str[0:6])
+print(str[45:71] + str[88:91] + str[0:6].replace("Python", " Python"))

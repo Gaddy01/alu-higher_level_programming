23c51c8454c5d1965e29f1e3876b153493ee1056
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-14 12:54:32 -0700

    task 3

--- python-hello_world/3-print_number.py
+++ python-hello_world/3-print_number.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 number = 98
-if str(number).isdigit():
+try number+1:
     print(f"{number} Battery street")
-else:
+except:
     print("ValueError: Unknown format code 'd' for object of type 'str'")

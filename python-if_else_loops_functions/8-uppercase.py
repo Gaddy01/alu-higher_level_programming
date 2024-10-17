417a5975c8b9df37f9b63a6d5705a5634870014c
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-17 02:47:02 -0700

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,12 @@
 
 
 def uppercase(str):
+    NUM1 = ord('a')
+    NUM2 = ord('z'
     for char in str:
         # Check if the character is a lowercase letter
-        if ord('a') <= ord(char):
-            if ord(char) <= ord('z'):
-                # Convert lowercase to uppercase by subtracting 32 from its ASCII value
-                char = chr(ord(char) - 32)
-                print("{}".format(char), end="")
+        if NUM1 <= ord(char) <= NUM2:
+            # Convert lowercase to uppercase by subtracting 32 from its ASCII value
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
     print()

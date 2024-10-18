5ba53c9d1fde0c33a871f955fae61a6c30ca3cf3
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-18 16:51:44 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 
 if __name__ == "__main__":
   a = 1
   b = 2
-  some = add_0.add(a,b)
+  some = add(a,b)
   
   print("{} + {} = {}".format(a, b, some))

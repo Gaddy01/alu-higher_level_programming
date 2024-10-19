52e4bbf976e964f52b2e8de9097b9c746fb3375a
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-19 10:08:52 +0200

    task 0

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,11 +5,11 @@ from calculator_1.py import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    some = add(a, b)
+    sum = add(a, b)
     substr = sub(a, b)
-    multip = mul(a, b))
+    multip = mul(a, b)
     divis = div(a, b)
-    print("{} + {} = {}".format(a, b, some))
+    print("{} + {} = {}".format(a, b, sum))
     print("{} - {} = {}".format(a, b, substr))
     print("{} * {} = {}".format(a, b, multip))
     print("{} / {} = {}".format(a, b, divis))

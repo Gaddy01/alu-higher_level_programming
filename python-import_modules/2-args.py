fac8eb6926b2bb470020304891747bd682cb780e
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-19 10:46:59 +0200

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,12 +5,12 @@ from sys import argv
 if __name__ == "__main__":
     num_arg = len(argv)-1
     if num_arg == 0: 
-        print("{} arguments".format(num_arg))
+        print("{} arguments.".format(num_arg))
         exit()
     elif num_arg == 1:
         print("{} argument".format(num_arg))
     else:
         print("{} arguments".format(num_arg))
 
-    for i in range(1, num_arg):
+    for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))

3f8fa5f61489980e60cecae0b8614571f82ae83f
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-19 01:31:53 -0700

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,9 +4,9 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        print("{} arguments".format(len(agv)))
+        print("{} arguments".format(len(argv)))
     else:
-        print("{} argument".format(len(agv)))
+        print("{} argument".format(len(argv)))
 
     for i in (1, len(argv)):
         print("{}: {}".format(i, argv[i]))

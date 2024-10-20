4c909e3bc2969fbf96243d8f1d561eb60fd1e6db
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-20 04:53:58 -0700

    task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    for i in range(len(my_list):
+        if i == idx:
+            continue
+        new_list.append(my_list[i])

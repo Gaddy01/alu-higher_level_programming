6dbc0b6cd95e8ab47334e73e8cb041f670ae3b63
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-20 11:32:32 +0200

    task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if 'my_list' in locals() or 'my_list' in globals():
+    if not my_list: 
+        return
+    else:
         reversed_list = my_list[::-1]
         for i in reversed_list:
             print("{:d}".format(i))
-    else:
-        return 

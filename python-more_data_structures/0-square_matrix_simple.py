c10f3e84171fd01d6f3efb6afdf3778ed1f8d3c1
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-21 03:04:49 -0700

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-    new_matrix = [ x*x for x in matrix]
+    new_matrix = [[x*x for x in liste] for liste in matrix]
     return new_matrix

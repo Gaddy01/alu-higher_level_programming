74c5f4772b7257b8e280721cecd7b1b8812a12f2
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-22 10:38:15 +0200

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,8 @@ def print_matrix_integer(matrix=[[]]):
     # Check if the matrix is truly empty or has empty sub-lists
     # if len(matrix) == 0 or (len(matrix) == 1 and len(matrix[0]) == 0):
     if matrix == [[]] or matrix == []:
-        return "\n"  # Return a newline for an empty matrix
+        print("\n")
+        return
     else:
         for liste in matrix:
             for i in liste:

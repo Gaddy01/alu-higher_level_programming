979f9042cc853177c87e151c4c8c065b62285256
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-22 02:55:09 -0700

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,8 @@ def roman_to_int(roman_string):
         return 0
     else:
         # Define the mapping of Roman numerals to integers
-        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                     'C': 100, 'D': 500, 'M': 1000}
         total = 0
         prev_value = 0
         # Iterate through the Roman numeral from right to left

0c8bd8c99ca1db1ea85eeaa0c4dc171c64760f38
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-28 20:52:43 +0200

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,11 @@
 
 
 def safe_print_division(a, b):
-     result = None
-     try:
-         result = a / b  # Perform the division
-     except ZeroDivisionError:
-         result = None  # Set result to None if division by zero occurs
-     finally:
-         print("Inside result: {}".format(result))  # Print the result in the finally block
-     return result  # Return the result (either the division or None)
+    result = None
+    try:
+        result = a / b  # Perform the division
+    except ZeroDivisionError:
+        result = None  # Set result to None if division by zero occurs
+    finally:
+        print("Inside result: {}".format(result))  # Print the result in the finally block
+    return result  # Return the result (either the division or None)

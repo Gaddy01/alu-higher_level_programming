bedc28fa8ba96129255725499273704bcf6cdf07
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-28 20:53:48 +0200

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -8,5 +8,6 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None  # Set result to None if division by zero occurs
     finally:
-        print("Inside result: {}".format(result))  # Print the result in the finally block
+        # Print the result in the finally block
+        print("Inside result: {}".format(result))
     return result  # Return the result (either the division or None)

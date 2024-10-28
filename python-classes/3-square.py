065dfd43516b8390a07ed2e196ef2db757c32d9b
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-28 23:00:04 +0200

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,6 +19,6 @@ class Square:
             self.__size = size
         except Exception as e:
             raise e
-        
+    # Creating a class method    
     def area(self,size):
-        return self.size**2
+        return self.__size**2

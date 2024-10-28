d94d707ac74eafd1e658ea9ea3b17b26ccdf32fd
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-28 22:51:05 +0200

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
     """
     def __init__(self, size=0):
         try:
-            if isinstance(size, int):
+            if not isinstance(size, int):
                 raise TypeError("size must be an integer")
             if size < 0:
                 raise ValueError("size must be >= 0")

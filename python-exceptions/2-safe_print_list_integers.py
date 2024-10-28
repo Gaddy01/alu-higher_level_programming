fa4df4a37b85f9c07edfde0d9471680bf180a7fe
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-28 11:26:15 -0700

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(0, int(x)):
+            print(my_list[i], end="")
+            count += 1
+    except:
+        pass
+    print()
+    return count

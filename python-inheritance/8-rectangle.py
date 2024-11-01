bcc29c701e0278dc29b4f6e3f85ca612dbe307b8
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 13:36:19 +0200

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,8 +13,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
-        if value <=0:
+        if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
 
 class Rectangle(BaseGeometry):
     """
@@ -24,7 +25,7 @@ class Rectangle(BaseGeometry):
         # Validate width and height using integer_validator
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        
+
         # Initialize width and height as private attributes
         self.__width = width
         self.__height = height

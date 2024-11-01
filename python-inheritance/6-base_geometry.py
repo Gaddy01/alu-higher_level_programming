b530647fa4252cf1f7005a3aa130845228b147ce
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:51:48 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+uhvbeijdskvniusahfwe ewijfewuhoiew
+wefwefhiewojfwpefwe
+wefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")

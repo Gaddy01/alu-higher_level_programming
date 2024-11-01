946806c731a94d785693ae5e5585433a5f08cf5d
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 04:28:04 -0700

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+uhvbeijdskvniusahfwe ewijfewuhoiew
+wefwefhiewojfwpefwe
+wefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    def __init__(self, width, height):
+        # Validate width and height using integer_validator
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        # Initialize width and height as private attributes
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <=0:
+            raise ValueError(f"{name} must be greater than 0")

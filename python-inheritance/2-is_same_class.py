2ca0e687f0222d368c95af7f68e2edf146a6735d
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:14:50 -0700

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+frhtwhytjykgcwr3rvteqwr
+bverybtyyumi6ew AEVRFUBIEWQONPeby65weasev
+vbtwbhfethyjuytre6rsedctyjew
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    fhowufibwfytvwyuweifoweyfteufewfwefwe
+    fwefewgergrthtrhtr
+    rthtyhjytjyyukiulktty
+    """
+    return type(obj) == a_class

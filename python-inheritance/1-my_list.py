46b39f8cd7f6abeacfa5daea24f20fda90cf35ce
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:02:57 -0700

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+the modeule is constituted by  a function that returns
+the list of available attributes and methods of the object.
+"""
+
+
+class MyList(list):
+    """
+    akhjwbgfgfefuhirjhggtg
+    rgrwgr
+    eg
+    regregrghrehtrhtrhjjju7ikytjtyej
+    ejrt
+    """
+    def print_sorted(self):
+        print(sorted(self))

e29e80257d58d79e50d1e6573be8ad26de275f10
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 02:47:25 -0700

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""
+the modeule is constituted by  a function that returns the list of available attributes and methods of an object.
+"""
+
+
+def lookup(obj):
+    return dir(obj)

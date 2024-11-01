f080524148ad0efc2b5e49e804558717715b220c
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:39:02 -0700

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+uyhg uiokadlgeag
+erqfrqegrqegerqgfAGBYKTJHAGFDCVYTERASD
+GREHSDGREHYTRNHDNhdgmjy,huytjhn jthgreht
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    fjie b8yg7ewqjhfsdbjkjsafdsb sfbefwberg
+    doiniomsdo,widvnmokwl;asdl,pqwasve
+    wdxinqafjkcwesa,dpqdk eiokwq[owe foiwk [qwdojwefwe
+    """
+    return isinstance(obj, a_class)

3b3627ea1873e37580c667167fad809b622ecc05
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:48:04 -0700

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+uhvbeijdskvniusahfwe ewijfewuhoiew
+wefwefhiewojfwpefwe
+wefwoeijfiweojfewf weofijwef
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class
+    """
+    pass

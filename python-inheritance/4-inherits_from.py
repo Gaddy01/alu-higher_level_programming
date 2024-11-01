76700a48acb490627f114cbf840ca92eb7a0563e
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-01 03:43:11 -0700

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+9fughebwufioew ngewni ewgoiqwjqjgqewg
+iqwgoqejgioejgeg egqgqeg
+gqwegewgegew gewqg
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    ofhbyguienwgewgrg  eg
+    gegqewgqegqegqe egqe
+    gqewgqeds eg  2grgfwef
+    """
+    return isinstance(obj, a_class)

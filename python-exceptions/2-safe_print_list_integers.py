9beb911a1f1d07696a216f93e1bc3ed0bdc9fb03
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-03 13:31:52 +0200

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,17 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(0, int(x)):
-            if str(my_list[i]).isdigit():
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            else:
-                continue
-    except:
-        pass
-    print()
+    count = 0  # Counts the number of integers printed
+    for i in range(x):
+        try:
+            # Attempt to print only integers
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            # Skip if the element is not an integer
+            continue
+        except IndexError:
+            # Stop if we go out of range
+            break
+    print()  # New line after printing all integers
     return count

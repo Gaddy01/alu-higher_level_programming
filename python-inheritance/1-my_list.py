d28fb0bb8c054fda8d97e6685f63b310382606ce
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-05 13:24:24 +0200

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""
-the modeule is constituted by  a function that returns
-the list of available attributes and methods of the object.
+""" This module creates a class, 'myList'
+    This module creates a class, 'myList'
+    This module creates a class, 'myList'
 """
 
 
 class MyList(list):
     """
-    akhjwbgfgfefuhirjhggtg
-    rgrwgr
-    eg
-    regregrghrehtrhtrhjjju7ikytjtyej
-    ejrt
+    MyList class that inherits from the built-in list class and .
+    This class is designed to represent a list with an added met.
     """
+
+    def __init__(self):
+        """
+        Initializes an instance of the MyList class.
+
+        Inherits from the built-in list class and does not require
+        """
+        super().__init__()  # Initialize the parent class (list)
+
     def print_sorted(self):
+        """
+        Prints the elements of the list in ascending sorted order.
+
+        Uses Python's built-in sorted() function to sort ult
+        without modifying the original list order.
+        """
         print(sorted(self))

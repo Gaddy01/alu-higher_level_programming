d1e9fe7eaf999586bc9798c40ddcc1b83951b374
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 19:48:21 +0200

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""
+ahf uianfhnjFGBLjsw F HgwadjkAFE
+QAdcwe teqwrecdeqw
+egwasfqwekciowqh32wqe
+"""
 
 
 def read_file(filename=""):
+    """
+    fhbuiwbfhcwqioadjm duhwqibdjno
+    i qwo duniwq iuqn
+    dq bdinqdhbcwuwaD
+    """
     with open(filename,"r") as file:
         content = file.read()
         print(content)

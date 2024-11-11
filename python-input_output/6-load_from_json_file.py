1a9a59997af23b999d24ed1f64689cb4989b10c1
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 11:38:21 -0800

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+ghuihfuewiqgjnrwegf
+grwgqewg wegqewgqewg3
+qwegfeqw geqwgew ge wqg3
+ewg eqG2 QGEG EGE
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    wuubfiewqf ewqfyhuqfhqwf
+    fq  fqwefbuhqweg qwgqwg
+    egqwegiqwgq qwefqewg
+    """
+    with open(filename, "r") as file:
+        json_object = file.read()
+        str_object = json.loads(json_object)
+        return str_object

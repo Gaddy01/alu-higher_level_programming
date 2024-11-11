8f4638a7b491cca9a674c7ef99efe36db750e03c
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 10:55:05 -0800

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+qwhduibyftvew fiwqugFBUYHASF
+QWsfcqbyuasbvuiweqnfv qwIDJOQWFAS
+wufgqbwjifknqwf qwf
+qfqwfnjqnwfqwfq
+"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    jihgfueiwqghfeiwog wesahviowasv
+    asvBASUVIJNWiaosjvwasv AC
+    ASVCQasfcjkqSANVC QSAFC
+    QSafcqadq
+    """
+    my_obj_json = json.dumps(my_obj)
+    return my_obj_json

7e5cf567b47d204d49628863b66dc5cb458b3b22
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 12:06:36 -0800

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+sdhjds sauhfuweb asfwa
+sfAFS Wqahfsb qwAFSHWQifj qAFSA
+SF sgdhioaskjfoklwasfonaw sdgflKASM
+"""
+
+
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+
+filename = "add_item.json"
+
+try:
+    # Try to load existing items from the file
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    # If the file doesn't exist, start with an empty list
+    items = []
+
+# Add new arguments to the list
+items.extend(sys.argv[1:])
+
+# Save the updated list back to the file
+save_to_json_file(items, filename)
+

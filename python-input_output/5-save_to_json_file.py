306c424a71e8de9f4e19ae3087be822c2b79f714
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 11:21:50 -0800

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+qwhduibyftvew fiwqugFBUYHASF
+QWsfcqbyuasbvuiweqnfv qwIDJOQWFAS
+wufgqbwjifknqwf qwf
+qfqwfnjqnwfqwfq
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    jihgfueiwqghfeiwog wesahviowasv
+    asvBASUVIJNWiaosjvwasv AC
+    ASVCQasfcjkqSANVC QSAFC
+    QSafcqadq
+    """
+    my_obj_json = json.dumps(my_obj)
+    with open(filename, "w") as file:
+        file.write(my_obj_json)

a60d03cc1f3b7a9bb2a52e185ce927c1a0077bcd
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-11 23:58:04 -0800

    task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+fijeuf89iewfew ewf32wf32wefsc
+wqfc8w9qf c2eiuqwifh9wq fqi1qeu90
+dh8buiwqgfb32 qwfuyuyqwh qweiq
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structures 
+    (list, dictionary, string, integer, and boolean) for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: A dictionary representation of the object's attributes.
+    """
+    return obj.__dict__

2fb0e8ef4a2c0da6f5190bdbca0cc39bcc45cf08
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-12 09:59:53 +0200

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,8 +8,9 @@ dh8buiwqgfb32 qwfuyuyqwh qweiq
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structures 
-    (list, dictionary, string, integer, and boolean) for JSON serialization of an object.
+    Returns the dictionary description with simple data structures
+    (list, dictionary, string, integer, and boolean) 
+    for JSON serialization of an object.
 
     Args:
         obj: An instance of a class.

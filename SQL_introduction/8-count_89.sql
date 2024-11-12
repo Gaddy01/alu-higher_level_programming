cba32ebc1d05157f9f95b41e7fc00505a5dff487
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-12 14:31:12 +0200

    task 8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,6 @@
 -- comment
-SELECT COUNT (*) FROM first_table WHERE id=89;
+-- SELECT COUNT (*) FROM first_table WHERE id=89
+-- Count the number of records with id = 89 in first_table
+SELECT COUNT(*) 
+FROM first_table 
+WHERE id = 89;

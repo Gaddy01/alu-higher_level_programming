a9f55cf5c5d5a898b1350737e70a8aa3a83a5846
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-14 15:25:36 +0200

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,7 @@
 -- comment
+SELECT tv_genres.name, COUNT(*) AS 'number_of_shows'
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;

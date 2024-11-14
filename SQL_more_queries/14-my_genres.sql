e2564df73ceca0fbd054f44fa20043780a4e5ade
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-14 15:41:04 +0200

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,7 @@
 -- Comment
-
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = 8
+ORDER BY tv_genres.name ASC

a5498dcb1c6b8873afb7528fab45e5b3a02c4bbc
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-15 11:46:46 +0200

    task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,7 +5,7 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 
-LEFT JOIN tv_genres
+INNER JOIN tv_genres
 ON tv_show_genres.show_id = tv_genres.id
 
 WHERE tv_shows.id = tv_show_genres.show_id

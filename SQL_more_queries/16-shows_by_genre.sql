5e1fdbfaa616d10a6232c60cfcc3d009ca27bcd2
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-15 11:44:51 +0200

    task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -8,5 +8,5 @@ ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres
 ON tv_show_genres.show_id = tv_genres.id
 
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_genres.name ASC
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name ASC;

c6fd0dd4035edb1241a788cabc2700d9ad879c88
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-15 11:49:11 +0200

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -5,8 +5,7 @@ FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 
-RIGHT JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.show_id = tv_genres.id
 
--- WHERE tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_genres.name ASC;

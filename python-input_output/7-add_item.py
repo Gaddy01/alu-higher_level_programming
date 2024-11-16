2e8fe0b21b415c79b8140d82ff8a6b06c17f3096
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-16 09:52:43 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
+"""7-add_item module"""
 
+
+import sys
+
+if _name_ == "_main_":
+    save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+    load_from_json_file = _import_('6-load_from_json_file') \
+        .load_from_json_file
+
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")

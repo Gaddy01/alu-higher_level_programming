d47ac0c45e18dd822c6678cccfd1306ec539acf3
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-16 10:27:31 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-"""7-add_item module"""
+"""
+This script adds all arguments passed to it to a Python list
+and saves the list to a JSON file.
+    - Uses save_to_json_file from 5-save_to_json_file.py
+    - Uses load_from_json_file from 6-load_from_json_file.py
+    - Saves the list in add_item.json
+"""
 
 
 import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
 
-if _name_ == "_main_":
-    save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
-    load_from_json_file = _import_('6-load_from_json_file') \
-        .load_from_json_file
 
-    try:
-        loadFile = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        loadFile = []
-
-    argc = len(sys.argv)
-    for idx in range(1, argc):
-        loadFile.append(sys.argv[idx])
-    save_to_json_file(loadFile, "add_item.json")
+filename = "add_item.json"
+try:
+    # Try to load existing items from the file
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    # If the file doesn't exist, start with an empty list
+    items = []
+# Add new arguments to the list
+items.extend(sys.argv[1:])
+# Save the updated list back to the file
+save_to_json_file(items, filename)

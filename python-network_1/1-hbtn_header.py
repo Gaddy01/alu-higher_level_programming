b61bb53921a5f2b257f18256f4f7609b289281b2
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-23 00:46:13 +0200

    task 1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,10 +6,11 @@ import sys
 # Get the URL from the command-line arguments
 url = sys.argv[1]
 
-# Send a request to the URL and fetch the response
-with urllib.request.urlopen(url) as response:
-    # Extract the value of the X-Request-Id from the headers
-    x_request_id = response.headers.get('X-Request-Id')
-
-# Display the value of the X-Request-Id variable
-print(x_request_id)
+if __name__ == "__main__":
+    # Send a request to the URL and fetch the response
+    with urllib.request.urlopen(url) as response:
+        # Extract the value of the X-Request-Id from the headers
+        x_request_id = response.headers.get('X-Request-Id')
+    
+    # Display the value of the X-Request-Id variable
+    print(x_request_id)

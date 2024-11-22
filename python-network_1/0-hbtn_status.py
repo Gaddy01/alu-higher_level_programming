7e99fb71351f17571179cb1a44f62bee006b5fbe
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-22 01:59:31 -0800

    task 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import urllib.request
+
+url = "https://alu-intranet.hbtn.io/status"
+
+# Fetch the URL using urllib
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+# Display the response details
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
+

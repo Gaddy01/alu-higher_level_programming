85cb0e3ed29b751ed109d063446ec1f7d224d362
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-22 12:35:16 +0200

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,29 +2,23 @@
 """
 This script sends a POST request to a specified URL with an email parameter
 and displays the body of the response, decoded in UTF-8.
-
 Usage:
     ./2-post_email.py <URL> <email>
-
 Arguments:
     <URL>   - The URL to which the POST request is sent.
     <email> - The email address to be sent as a parameter in the POST request.
-
 Features:
     - Utilizes the `urllib` module for HTTP requests.
     - Sends the email as a parameter in the POST request.
     - Decodes and displays the response body in UTF-8.
     - Employs a `with` statement to ensure proper resource management.
-
 Requirements:
     - Only `urllib` and `sys` modules are used.
     - The script assumes valid command-line arguments are passed.
-
 Example:
     $ ./2-post_email.py http://localhost:5000/email test@example.com
     Your email is: test@example.com
 """
-
 import urllib.parse
 import urllib.request
 import sys

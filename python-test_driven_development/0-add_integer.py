c12918529493d9af29f7a82a863dbdd8089495c2
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-23 03:22:07 -0800

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+This module adds two integers
+"""
+
+def add_integer(a, b=98):
+    """Write a function that adds 2 integers."""
+     # Validate that a is an integer or float
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    # Validate that b is an integer or float
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # Cast a and b to integers and return their sum
+    return int(a) + int(b)

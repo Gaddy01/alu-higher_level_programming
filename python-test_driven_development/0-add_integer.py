cf2e9d7dfbffd063749764331ea7f6967c967135
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-23 14:26:00 +0200

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,7 +14,7 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    if a+1 == a or b +1 == b:
+    if a + 1 == a or b + 1 == b:
         raise OverflowError("number too large")
 
     # Cast a and b to integers and return their sum

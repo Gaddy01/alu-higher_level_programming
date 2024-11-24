d4315cacfac540db7f7cb5a3e558db5304dce753
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-24 03:40:09 -0800

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,3 +8,9 @@ class TextMaxInteger(unittest.TestCase):
     def test_integer(self):
         # Test if the list is empty
         self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1,5,3,9]), 9)
+        self.assertAlmostEqual(max_integer([10,5,2,8]), 10)
+        self.assertAlmostEqual(max_integer([5,8,3]), 8)
+        self.assertAlmostEqual(max_integer([4,-8,19,2]), 19)
+        self.assertAlmostEqual(max_integer([-101,-45,-78,-96,-10,-175]), -10)
+        self.assertAlmostEqual(max_integer([6]), 6)

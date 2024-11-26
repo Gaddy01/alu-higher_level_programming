5ff955bb65e4cc072d382548232364d3e79b4d7e
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-26 12:15:38 +0200

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -125,3 +125,17 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Return the dictionary representation of the Rectangle.
+        Returns:
+            dict: A dictionary containing the attributes of the rectangle.
+        """
+        return {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }

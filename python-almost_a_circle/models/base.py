eb521044ab4421c9ae818d8522d303487008d157
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-26 12:29:22 +0200

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 This is the module documentation. The module create a class Base
 """
 
+import json
+
 
 class Base:
     """Base class for managing IDs."""
@@ -21,3 +23,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+
+        Returns:
+            str: JSON string representation of list_dictionaries or "[]" if None or empty.
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)

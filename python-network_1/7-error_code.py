dfa4d91cd0869ea15611e1c7e1a86f030098e09c
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-27 12:17:10 +0200

    Update 7-error_code.py

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -19,7 +19,9 @@ import sys
 
 
 if __name__ == "__main__":
-    # Get the URL from the command-line argument
+    """
+    Get the URL from the command-line argument
+    """
     url = sys.argv[1]
 
     # Send a GET request to the URL

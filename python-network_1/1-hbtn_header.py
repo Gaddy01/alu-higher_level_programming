8b3d16ede44b92e51ca02f21ba928cccea15d422
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-29 15:25:49 +0200

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """
-This script prints the headers of the response
-"""
-import sys
+Fetch and print the 'X-Request-Id' header from a URL's HTTP response.
 
+This script takes a URL as a command-line argument, sends a GET request to
+the URL, and extracts the value of the 'X-Request-Id' header from the response.
+"""
+
+import sys
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.getheader("X-Request-Id"))
+    # Get the URL from the command-line arguments
+    url = sys.argv[1]
+
+    # Send a request to the URL and fetch the response
+    with urllib.request.urlopen(url) as response:
+        # Extract the value of the X-Request-Id from the headers
+        x_request_id = response.getheader('X-Request-Id')
+        # Display the value of the X-Request-Id variable
+        print(x_request_id)

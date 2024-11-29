5efb13f8d9ee7ff5c81a140ef15e0f20edc0a2d5
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-29 15:11:11 +0200

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     # Send a request to the URL and fetch the response
     with urllib.request.urlopen(url) as response:
         # Extract the value of the X-Request-Id from the headers
-        x_request_id = response.headers.get('X-Request-Id')
-    
-    # Display the value of the X-Request-Id variable
-    print(x_request_id)
+        x_request_id = response.getheader('X-Request-Id')
+        # Display the value of the X-Request-Id variable
+        print(x_request_id)

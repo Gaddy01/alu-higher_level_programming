7f5e861fefb2566d4b43c2cee730ccd65678c130
Gaddy01 <g.irakoze2@alustudent.com>
2024-11-29 15:52:43 +0200

    Update 3-error_code.py

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -7,15 +7,10 @@ import sys
 import urllib.request
 import urllib.error
 
-
 if __name__ == "__main__":
-    # Read the URL from the command-line arguments
     url = sys.argv[1]
     try:
-        # Open the URL and read the response
         with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-     except urllib.error.HTTPError as e:
-         # Handle HTTP errors and print the error code
-         print(f"Error code: {e.code}")
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)

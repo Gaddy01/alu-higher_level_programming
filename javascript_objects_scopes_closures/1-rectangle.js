4d2b58d88206d45b494eab77b66843ddfa8a402b
Gaddy01 <g.irakoze2@alustudent.com>
2025-02-06 02:31:52 -0800

    commit

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) { // Add space between "constructor" and "("
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  charPrint (c = 'X') {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
